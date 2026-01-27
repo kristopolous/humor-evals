0093e2e184fda6ef0f18f22015acf2a42b2128d1
Chris McKenzie <kristopolous@yahoo.com>
2026-01-27 02:47:15 -0800

    updating the eval list

--- joke-eval.sh
+++ joke-eval.sh
@@ -3,17 +3,19 @@ server="http://localhost:11434"
 for model in $(llcat -su $server -m | sort); do
     printf "%-40s," $model
     score=0
-    for i in $(seq 1 75); do
+    for i in $(seq 1 80); do
         all=$(./joke_eval.py $i)
         correct=${all:0:1}
-        guess=$(timeout 60s llcat -u $server -m "$model" "${all:2}" | tr -cd 'AB' )
+        guess=$(timeout 300s llcat -u $server -m "$model" "${all:2}" | tr -cd 'AB' )
         guess=${guess:0:1}
         [[ -n "$guess" ]] || guess="X"
 
-        echo -n "$guess"
 
         if [[ "$guess" == "$correct" ]]; then
+            echo -n "."
             (( score++ ))
+        else
+            echo -n "!"
         fi
     done
     echo ", $(( 100 * score / i )), $model"
