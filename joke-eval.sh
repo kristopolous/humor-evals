9225de00b3eab5870d921d57142e5eccda149431
Chris McKenzie <kristopolous@yahoo.com>
2026-01-27 12:29:44 -0800

    more trials

--- joke-eval.sh
+++ joke-eval.sh
@@ -3,7 +3,7 @@ server="http://localhost:11434"
 for model in $(llcat -su $server -m | sort); do
     printf "%-40s," $model
     score=0
-    for i in $(seq 1 80); do
+    for i in $(seq 1 250); do
         all=$(./joke_eval.py $i)
         correct=${all:0:1}
         guess=$(timeout 300s llcat -u $server -m "$model" "${all:2}" | tr -cd 'AB' )
